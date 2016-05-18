de6c97af91cd2a3629c495947036d6832242edf2
Karuppiah <karuppiah7890@gmail.com>
2016-05-18 18:34:16 +0530

    Moved test.js

---
+++ test.js
@@ -0,0 +1,10 @@
+var path = require('path');
+var merge = require('.' + path.sep + 'index.js');
+
+merge(['test' + path.sep + 'github\ cheat sheet.pdf','test' + path.sep + 'text_extraction.pdf'],'test' + path.sep + 'Out.pdf',function(err){
+
+          if(err)
+          return console.log(err);
+
+          console.log("Success");
+});
--- test/test.js
+++
@@ -1,9 +0,0 @@
-var merge = require('../lib/PDFMerger');
-
-merge(['test/github cheat sheet.pdf','test/text_extraction.pdf'],'test/Out.pdf',function(err,stdout,stderr){
-
-          if(err)
-          return console.log(err);
-
-          console.log("Success");
-});
