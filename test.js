77f6c57b4d833146e7863fdad75bbb14718e4954
Karuppiah <karuppiah7890@gmail.com>
2016-05-21 22:46:46 +0530

    Use template literals. Better coding style

--- test.js
+++ test.js
@@ -1,7 +1,7 @@
 var path = require('path');
-var merge = require('.' + path.sep + 'index.js');
+var merge = require(`./index.js`);
 
-merge(['test' + path.sep + 'github cheat sheet.pdf','test' + path.sep + 'text_extraction.pdf'],'test' + path.sep + 'Out.pdf',function(err){
+merge([`test${path.sep}github cheat sheet.pdf`,`test${path.sep}text_extraction.pdf`],`test${path.sep}Out.pdf`,function(err){
 
           if(err)
           return console.log(err);
