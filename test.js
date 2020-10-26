835481caef34f058021658eb747ab7d9cbf74666
Karuppiah Natarajan <karuppiah7890@gmail.com>
2020-10-26 21:54:58 +0530

    add opts to the test file

--- test.js
+++ test.js
@@ -1,7 +1,12 @@
 var path = require('path');
 var merge = require(`./source/index.js`);
 
-merge([`test${path.sep}github cheat sheet.pdf`, `test${path.sep}text_extraction.pdf`], `test${path.sep}Out.pdf`, function (err) {
+const opts = {
+  maxBuffer: 1024 * 500, // 500kb
+  maxHeap: '10m' // for setting JVM heap limits
+}
+
+merge([`test${path.sep}github cheat sheet.pdf`, `test${path.sep}text_extraction.pdf`], `test${path.sep}Out.pdf`, opts, function (err) {
 
   if (err)
     return console.log(err);
